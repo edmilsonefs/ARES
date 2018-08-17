b54c3ded9447651d2460bc4ba31d835b57646c24
Herbertt <hdiniz@Coffee.lan>
2018-08-17 18:52:14 -0400

    edited readme

--- ProducerCEPKafka/src/main/java/MainProducerCEP2.java
+++ ProducerCEPKafka/src/main/java/MainProducerCEP2.java
@@ -4,32 +4,36 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.kafka.core.KafkaTemplate;
 
-public final class MainProducerCEP2 {
+public final class MainProducerCEP3 {
 
-	private MainProducerCEP2() { }
+	private MainProducerCEP3() { }
 
 public static void main(final String... args) throws InterruptedException {
+        
 
-		
-	@SuppressWarnings("resource")
-	final AbstractApplicationContext context =	new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-	KafkaTemplate<String, String> kafkaTemplate = (KafkaTemplate) context.getBean("template");
-	kafkaTemplate.setDefaultTopic("si.ceprule.queue");
-
+		@SuppressWarnings("resource")
+		final AbstractApplicationContext context =	new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		KafkaTemplate<String, String> kafkaTemplate = (KafkaTemplate) context.getBean("template");
+		kafkaTemplate.setDefaultTopic("si.ceprule.queue");
+	   
 
 	    String payload = "";
-	
-	    payload = "{\"contextElement\":{\"type\" : \"RULECEP\",\"id\" : \"Rule2\","
-	    		+ "\"attributes\" : [{ \"name\"  : \"RULE\","
-	    		                    + "\"type\"  : \"String\","
-	    		                    + "\"value\" : \"select id from Entity.win:time(5 sec) where cast(id, double)= 2\"}"
-	    		                    + "{\"name\" : \"QUEUE_2\","
-	    	    		            + "\"type\"  : \"QUEUE\","
-	    	    		            + "\"value\" : \"si.cep.queue2\"}"
-	    		                    + "]}}";
+	    //Terceiro passo
+	    //O tipo RULECEP indica REGRA a ser Adicionada o typo QUEUE indica que tera uma fila de destino no broker
+	    payload = "{\"contextElement\":{\"type\" : \"EDIT_RULECEP\",\"id\" : \"Rule3\","
+			    		+ "\"attributes\" : [{ \"name\"  : \"RULE_ID\","
+		                + "\"type\"  : \"String\","
+		                + "\"value\" : \"cf943226-be26-4e80-a9ad-516554ddcfbe\"}"
+		                + "{\"name\"  : \"RULE\","
+			            + "\"type\"  : \"String\","
+			            + "\"value\" : \"select temperature from Termometro.win:time(10 sec)\"}"
+			            + "{\"name\"  : \"QUEUE_1\","
+    		            + "\"type\"  : \"QUEUE\","
+    		            + "\"value\" : \"si.cep.queue2\"}"
+	                    + "]}}";
 	
 	    kafkaTemplate.sendDefault(payload);
 	    context.registerShutdownHook();
-	    System.out.println("ProducerCEP2");
+	    System.out.println("ProducerCEP3");
 	}
 }
